4050929066ab5661b68243e5eeb9abd8b03640c3
LeNightmare <qjjmail@126.com>
2018-04-17 00:33:05 -0400

    submit

---
+++ lt/lt693.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def hasAlternatingBits(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        last = "2"
+        for i in str(bin(n))[2:]:
+            if last == i:
+                return False
+            last = i
+        return True
+
+print(Solution.hasAlternatingBits(Solution(), 4))
